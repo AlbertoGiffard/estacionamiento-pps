5206e7fa23b48fb756f75d83fce5ffc531fdabea
AlbertoGiffard <albertoggm1@gmail.com>
2023-04-11 22:21:54 -0300

    updates

--- src/App.js
+++ src/App.js
@@ -2,16 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import firebase from "firebase/app";
+import "firebase/firestore";
 import Header from './componentes/header/Header';
 import Inicio from './componentes/inicio/Inicio';
 import Footer from './componentes/footer/Footer';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyB8CybzZByE7AMyTyyATPqRj2PtMvoR7eM",
+  authDomain: "pps-6to-semestre.firebaseapp.com",
+  projectId: "pps-6to-semestre",
+  storageBucket: "pps-6to-semestre.appspot.com",
+  messagingSenderId: "445817961783",
+  appId: "1:445817961783:web:cb7d61ff2e62619c02aeb2"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Inicio/>
-      <Footer/>
+      <Header />
+      <Inicio />
+      <Footer />
     </div>
   );
 }
